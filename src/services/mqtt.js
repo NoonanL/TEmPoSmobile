ae25ff983d260c2352e2d14923849fa99247195f
Liam Noonan <contactliamnoonan@gmail.com>
2019-03-24 22:57:48 +0000

    Fixed MQTT implementation to allow for reconnection after LoS and reattempting any message sending.

--- src/services/mqtt.js
+++ src/services/mqtt.js
@@ -12,30 +12,55 @@ export const publishMQTT = (topic, publishMessage, address) => {
     storageBackend: AsyncStorage,
     defaultExpires: 1000 * 3600 * 24,
     enableCache: true,
-    reconnect: true,
     sync: {}
   });
 
+  //On connection established with mosquitto broker
   function onConnect() {
     console.log('Connected to Broker');
     //Send message to broker (topic, message, QoS)
-    client.send(topic, publishMessage, 1);
+    client.publish(topic, publishMessage, 2);
   }
 
+  //On Message delivery confirmed
+  function onMessageDelivered() {
+    console.log('Message was delivered, disconnecting from broker.');
+  }
+
+  //On Connection lost
   function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
-      console.log('Connection to broker lost!');
+      console.log(
+        'Connection to broker lost! Error code: ' + responseObject.errorCode
+      );
     }
   }
 
+  //On attempt to connect failure
+  function onFailure() {
+    console.log('Failed to connect to broker, will attempt to reconnect...');
+  }
+
+  //On Message Arrives
   function onMessageArrived(message) {
     console.log('Message arrived from Broker: ' + message.payloadString);
   }
 
-  //console.log(address);
+  //Create client object and set it's handlers.
   const client = new Paho.MQTT.Client(address, 1884, '/ws', 'ReactNative');
-  // console.log(client);
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
-  client.connect({ onSuccess: onConnect });
+  client.onMessageDelivered = onMessageDelivered;
+
+  //Connect to the Mosquitto Broker
+  client.connect({
+    onSuccess: onConnect,
+    onFailure: onFailure,
+    /*
+    This is REALLY important. You have to set this to false so that the broker tries to reconnect
+     and persists the message for the client until the reconnection from this particular client is noted
+    */
+    cleanSession: false,
+    reconnect: true
+  });
 };
